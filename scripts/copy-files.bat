27a97db192b2c3d19aacadd032685e4b48f2a460
Sunguk Lee <d3m3vilurr@gmail.com>
2026-02-14 05:42:42 +0900

    update auto install script

--- scripts/copy-files.bat
+++ scripts/copy-files.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Deleting old files
-rm -R "C:\Program Files (x86)\Diablo II Resurrected\mods\Reimagined\Reimagined.mpq\data"
+rm -R "..\Reimagined\Reimagined.mpq\data"
 echo Copying files
-robocopy "C:\dev\d2r\d2r-reimagined\data" "C:\Program Files (x86)\Diablo II Resurrected\mods\Reimagined\Reimagined.mpq\data" /MIR /E /IS /IT
+robocopy ".\data" "..\Reimagined\Reimagined.mpq\data" /MIR /E /IS /IT
